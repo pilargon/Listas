ffab91ab5e79a4fafcb3cc735bee8d666a0ff1b0
Pilar G <pilargonzalezcla@gmail.com>
2018-10-18 11:04:01 +0200

    final

---
+++ Numeros/Numeros/CantanteFamoso.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numeros
+{
+    class CantanteFamoso
+    {
+        //La clase debe tener un método que permita añadir objetos de tipo CantanteFamoso a la lista
+       private string nombre;
+       private string discoConMasVentas;
+        string cantante3;
+        string disco3;
+
+        public CantanteFamoso()
+        {
+            
+        }
+        public CantanteFamoso(String nombre, String discoConMasVentas)
+        {
+            this.nombre = nombre;
+            this.discoConMasVentas = discoConMasVentas;
+        }
+
+        public String GetNombre()
+        {
+            return nombre;
+        }
+        public void SetNombre(String nombre)
+        {
+            this.nombre = nombre;
+        }
+        public String GetdiscoConMasVentas()
+        {
+            return discoConMasVentas;
+        }
+        public void SetdiscoConMasVentas(String discoConMasVentas)
+        {
+            this.discoConMasVentas = discoConMasVentas;
+        }
+
+
+       
+
+       
+
+    }
+}
--- Numeros/Numeros/Program.cs
+++ Numeros/Numeros/Program.cs
@@ -13,74 +13,139 @@ namespace Numeros
           // Si el número introducido es decimal lo guardará en una lista de números decimales,.
           //Si no es decimal entonces los guardará en una lista de ints.
 
-            List<int> Entero = new List<int>();
-            List<double> Decimal = new List<double>();
-            IntroducirNum(Entero, Decimal);
-            MostrarListas(Entero, Decimal);
-            Console.WriteLine("El numero de enteros es: " + ContarListasE(Entero));
-            Console.WriteLine("El numero de decimales es: " + ContarListasD(Decimal));
-            Console.WriteLine("La suma de los enteros es: " + SumarListasE(Entero));
-            Console.WriteLine("La media de todos los numeros es: " + (SumarListasE(Entero)+ SumarListasD(Decimal))/ (ContarListasE(Entero)+ ContarListasD(Decimal)));
+            //List<int> Entero = new List<int>();
+            //List<double> Decimal = new List<double>();
+            //IntroducirNum(Entero, Decimal);
+            //MostrarListas(Entero, Decimal);
+            //Console.WriteLine("El numero de enteros es: " + ContarListasE(Entero));
+            ////Console.ForegroundColor = ConsoleColor.DarkRed;
+            //Console.WriteLine("El numero de decimales es: " + ContarListasD(Decimal));
+            //Console.WriteLine("La suma de los enteros es: " + SumarListasE(Entero));
+            //Console.WriteLine("La media de todos los numeros es: " + (SumarListasE(Entero) + SumarListasD(Decimal)) / (ContarListasE(Entero) + ContarListasD(Decimal)));
+
+            List<CantanteFamoso> listaCantantesFamosos = new List<CantanteFamoso>();
+            CrearLista(listaCantantesFamosos);
+            MostrarLista(listaCantantesFamosos);           
+            Insertar(listaCantantesFamosos);
             
-
-            //Console.WriteLine("La lista de enteros es: " + Entero);
-            //Console.WriteLine("La lista de enteros es: " + Decimal);
             Console.ReadLine();
-        }
 
-        public static void IntroducirNum(List<int> Entero, List<double> Decimal)
+        }
+        public static void CrearLista(List<CantanteFamoso> listaCantantesFamosos)
         {
-            string num;
+            CantanteFamoso cantante1 = new CantanteFamoso();
+            cantante1.SetNombre("Falete");
+            cantante1.SetdiscoConMasVentas("Disco de Falete");
+            listaCantantesFamosos.Add(cantante1);
+            CantanteFamoso cantante2 = new CantanteFamoso();
+            cantante2.SetNombre("Camaron");
+            cantante2.SetdiscoConMasVentas("Disco de Camaron");
+            listaCantantesFamosos.Add(cantante2);
+        }
+        public static void MostrarLista(List<CantanteFamoso> listaCantantesFamosos)
+        {
+            foreach (CantanteFamoso x in listaCantantesFamosos)
+            {
+                Console.WriteLine(x.GetNombre());
+                Console.WriteLine(x.GetdiscoConMasVentas());
+            }
+        }
+        public static void Insertar(List<CantanteFamoso> listaCantantesFamosos)
+        {
+            string respuesta;
             do
             {
-                Console.WriteLine("Dame numeros positivos");
-                num = Console.ReadLine();
+                CantanteFamoso cantante3 = new CantanteFamoso();
 
-                if (num.Contains(".") || num.Contains("'"))
-                {
-                    Decimal.Add(Convert.ToDouble(num.Replace(".", ",")));//No me deja poner coma por teclado,
-                    //y al tranformarlo en numero me quita la coma,asi que reemplazmos por punto
-                }
-                else if (!num.Contains(".") && !num.Contains("'") && !num.Contains("0") && !num.Contains("-"))
-                {
-                    Entero.Add(Convert.ToInt32(num));
-                }
-                else //TODO: JAJAJ
-                {
-                    Console.WriteLine("Has introducido una letra");
-                }
-            }
-            while (Convert.ToDouble(num) >= 0);
-        }
+                Console.WriteLine("\nDime un cantante famoso");
+                cantante3.SetNombre(Console.ReadLine());
+                listaCantantesFamosos.Add(cantante3);
 
-        public static void MostrarListas(List<int> Entero, List<double> Decimal)
-        {
-            Console.WriteLine("La lista de enteros es: ");
-            foreach (int x in Entero)
-            {
-                Console.WriteLine(x);
+                Console.WriteLine("Dime un disco del famoso");
+                cantante3.SetdiscoConMasVentas(Console.ReadLine());
+                listaCantantesFamosos.Add(cantante3);
+
+                Console.WriteLine("Quieres seguir escribiendo cantantes?:S/N");
+                respuesta= Console.ReadLine();
             }
-            Console.WriteLine("La lista de decimales es: ");
-            foreach (double x in Decimal)
-            {
-                Console.WriteLine(x);
-            }
+            while (respuesta.ToUpper() == "S");          
+               
         }
-        public static int ContarListasE(List<int> Entero)
-        {
-            return Entero.Count();
-        }
-        public static double ContarListasD(List<double> Decimal)
-        {
-            return Decimal.Count();
-        }
-        public static int SumarListasE(List<int> Entero)
-        {
-            return Entero.Sum();
-        }
-        public static double SumarListasD(List<double> Decimal)
-        {
-            return Decimal.Sum();
-        }
+       
+        //public static void IntroducirNum(List<int> Entero, List<double> Decimal)
+        //{
+        //    string num;
+        //    do
+        //    {
+        //        Console.WriteLine("Dame numeros positivos");
+        //        num = Console.ReadLine();
+
+        //        if (num.Contains(".") || num.Contains("'"))
+        //        {
+        //            Decimal.Add(Convert.ToDouble(num.Replace(".", ",")));//No me deja poner coma por teclado,
+        //            //y al tranformarlo en numero me quita la coma,asi que reemplazmos por punto
+        //        }
+        //        else if (!num.Contains(".") && !num.Contains("'") && !num.Contains("0") && !num.Contains("-"))
+        //        {
+        //            Entero.Add(Convert.ToInt32(num));
+        //        }
+        //        //else //TODO: JAJAJ
+        //        //{
+        //        //    Console.WriteLine("Has introducido una letra");
+        //        //}
+        //    }
+        //    while (Convert.ToDouble(num) >= 0);
+        //}
+
+        //public static void MostrarListas(List<int> Entero, List<double> Decimal)
+        //{
+        //    Console.WriteLine("La lista de enteros es: ");
+        //    foreach (int x in Entero)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //    Console.WriteLine("La lista de decimales es: ");
+        //    foreach (double x in Decimal)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //}
+        //public static int ContarListasE(List<int> Entero)
+        //{
+        //    return Entero.Count();
+        //}
+        //public static double ContarListasD(List<double> Decimal)
+        //{
+        //    return Decimal.Count();
+        //}
+        //public static int SumarListasE(List<int> Entero)
+        //{
+        //    return Entero.Sum();
+        //}
+        //public static double SumarListasD(List<double> Decimal)
+        //{
+        //    return Decimal.Sum();
+        //}
+
+
+
     }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
